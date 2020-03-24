ff333baf6762763e3ceec6c15b93c004a619e31e
Jon <jong649@gmail.com>
2020-03-23 22:43:30 -0700

    first stage of adding parallax scrolling component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Parallax from "./components/Parallax/Parallax";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Parallax />
+      <div style={{ height: "800px", background: "red" }}>
+        <p>Here is more stuff</p>
+      </div>
     </div>
   );
 }
---
+++ src/components/Parallax/Parallax.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Parallax.css";
+
+function Parallax(props) {
+  return <div className="parallax"></div>;
+}
+
+export default Parallax;

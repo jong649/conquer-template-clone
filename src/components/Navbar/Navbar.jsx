f191065b45f6daa35636c0ab883fe3a304ad9302
Jon <jong649@gmail.com>
2020-03-23 21:54:09 -0700

    add menu icon and more media queries

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,12 +3,19 @@ import NavbarLink from "../NavbarLink/NavbarLink";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const handleClick = () => {
+    const navbarContainer = document.querySelector(".navbar-container");
+    navbarContainer.classList.toggle("small");
+  };
   return (
     <nav>
       <div className="navbar-container">
-        <ul>
+        <ul className="nav-ul">
           <NavbarLink title="Home" />
         </ul>
+      </div>
+      <div className="menu-container">
+        <button className="menu" onClick={handleClick}></button>
       </div>
     </nav>
   );
